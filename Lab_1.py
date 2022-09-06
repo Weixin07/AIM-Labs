b273265df1e8637768d2ce5a1160999a07f1488d
Weixin07 <77734173+Weixin07@users.noreply.github.com>
2022-09-06 10:51:38 +0800

    Initial commit

---
+++ Lab_1.py
@@ -0,0 +1,59 @@
+# scipy
+from tokenize import PlainToken
+import scipy
+print('scipy: %s' % scipy.__version__)
+# numpy
+import numpy
+print('numpy: %s' % numpy.__version__)
+# matplotlib
+import matplotlib
+print('matplotlib: %s' % matplotlib.__version__)
+# pandas
+import pandas
+print('pandas: %s' % pandas.__version__)
+# statsmodels
+import statsmodels
+print('statsmodels: %s' % statsmodels.__version__)
+# scikit-learn
+import sklearn
+print('sklearn: %s' % sklearn.__version__)
+
+# Load libraries
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+import pandas as pd
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pd.read_csv(url, names=names)
+print(dataset.shape)
+
+print(dataset.head(15))
+
+print(dataset.describe())
+
+print(dataset.groupby('class').size())
+
+dataset.hist()
+
+from pandas.plotting import scatter_matrix
+scatter_matrix(dataset)
+
+import matplotlib.pyplot as plt 
+plt.plot(dataset.groupby('class').size())
+plt.ylabel('Petals')
+plt.show()
